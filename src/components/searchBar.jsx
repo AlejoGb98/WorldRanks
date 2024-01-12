7a570c08794af9a43207ab93ac6e8b79c962fb73
AlejoGb98 <gallandalejo@gmail.com>
2024-01-11 21:07:18 -0300

    Search result correction
    In the search bar, i correct that if you dont search anything, the list of results will close

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
     const [search, setSearch] = useState('');
     const [searchResult, setSearchResult] = useState()
 
-
+    console.log(searchResult)
     //Fetch data to Searchbar
     const handleSearch = (value) => {
         getDataSearch(value)
@@ -20,9 +20,9 @@ const SearchBar = () => {
     async function getDataSearch(value){
         const resp = await fetch(`https://restcountries.com/v3.1/all`);
         const res = await resp.json();
-      
+
         const searchResults = res.filter((country) => {
-          return value && country && country.name.common.toLowerCase().includes(value.toLowerCase()) || country.region.toLowerCase().includes(value.toLowerCase()) /* || country.subregion.toLowerCase().includes(value.toLowerCase()) */
+          return value && country && country.name.common.toLowerCase().includes(value.toLowerCase()) || country.region.toLowerCase().includes(value.toLowerCase()) || country.subregion?.toLowerCase().includes(value.toLowerCase())
         })
         
         setSearchResult(searchResults)
@@ -32,18 +32,20 @@ const SearchBar = () => {
     <div className={`flex gap-2 bg-darkgrey py-2 w-2/6 right-0 text-lightgrey relative z-20 mb-4 ${search ? 'rounded-t-xl' : 'rounded-xl'}`}>
         <Image src={Search} width={25} height={25} alt='Search' className='mx-2'/>
         <input type='text' placeholder='Search by Name, Region, Subregion' value={search}
-                className='placeholder-lightgrey text-sm font-medium bg-darkgrey w-full outline-none mx-2'
+                className={`placeholder-lightgrey text-sm font-medium bg-darkgrey w-full outline-none mx-2 ${search && 'text-white'}`}
                 onChange={ e => handleSearch(e.target.value)}/>
         
-        <div className={`absolute w-full top-10 bg-darkgrey max-h-96 overflow-scroll rounded-b-xl ${searchResult?.length > 0 ? 'border-t-2 border-lightgrey' : ''}`}>
+        <div className={`absolute w-full top-10 bg-darkgrey max-h-96 overflow-scroll rounded-b-xl ${search && 'border-t-2 border-lightgrey'}`}>
             {
                 search && searchResult?.length === 0 ?
-                    <p className={`text-white px-6 py-3 ${search ? 'border-t-2 border-lightgrey' : ''}`}>No matches found</p>
+                    <p className={`text-white px-6 py-3 ${!search && 'border-t-2 border-lightgrey'}`}>No matches found</p>
                     :
                     searchResult?.map( country => (
                         <SearchResult
                             name={country.name.common}
                             flag={country.flags.svg}
+                            key={country.name.common}
+                            search={search}
                         />
                     ))
                     
--- src/components/searchResult.jsx
+++ src/components/searchResult.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const SearchResult = ({name, flag}) => {
+const SearchResult = ({name, flag, search}) => {
     
+  
+
   return (
     <Link href={`/country/${name}`}>
-    <div className="flex w-full gap-6 px-6 py-3 items-center">
-        <Image src={flag} height={1000} width={1000} className="w-14 h-10 object-cover aspect-video rounded-md" alt={`${name} flag`}/>
+    <div className={`flex w-full gap-6 px-6 py-3 items-center ${!search && 'hidden'}`}>
+        <Image src={flag} height={1000} width={1000} className='w-14 h-10 object-cover aspect-video rounded-md' alt={`${name} flag`}/>
         <p className="text-white">{name}</p>
     </div>
     </Link>
